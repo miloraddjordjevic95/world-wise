4bd44a60b52174c8f5394b12a3a019d1cae3437e
Milorad Djordjevic <lemybg95@gmail.com>
2024-02-05 18:48:42 +0100

    Finished

---
+++ src/constants/strings.js
@@ -0,0 +1,5 @@
+const STRINGS = {
+    BASE_URL: "https://api.bigdatacloud.net/data/reverse-geocode-client",
+};
+
+export default STRINGS;
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import {StrictMode} from "react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 )
